22ddb9da081dc36b7701e2faeb3f8cb9be8f81c2
Janani <105300710+Jananiksha@users.noreply.github.com>
2023-08-05 11:47:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import './index.js';
+import image from './bubble.jpg';
+
 
 
 function App() {
   return (
-    <>
-    <h1 style="color:black">WELCOME TO MY WEBSITE</h1>
+<div>
+    <h1>WELCOME TO MY WEBSITE</h1>
     <div>
-        <h2 class="p.padTop" style="color: black;">
+        <h2>
             This is the place where i can showcase my profile
         </h2>
         
     </div>
-    <h3 style="color: black;"> 
+    <h3> 
        I am Jananiksha
     </h3>
-    <p style="color: black;">
+    <p>
         Enthusiastic AI&DS student dedicated to lifelong learning and growth in the field of AI and Data Science.
     </p>
     <div>
-        <p style="color:black">
+        <p>
             <b>My Education:</b>
-            <p style="padding-left: 5%;"> ABC School</p>
-            <p style="padding-left: 5%;"> Kumaraguru College of Technology</p>
+            <p>ABC School </p>
+            <p> Kumaraguru College of Technology</p>
         </p>
 
-        <p style="color: black;">
+        <p>
             <b>My Interests:</b>
-            <p style="padding-left: 5%;">Listening to Music</p>
-            <p style="padding-left: 5%;">Reading books</p>
-            <p style="padding-left: 5%;">Travel</p>
+            <p>Listening to Music</p>
+            <p>Reading books</p>
+            <p>Travel</p>
         </p>
 
         <p>
@@ -40,36 +41,36 @@ function App() {
     </div>
     <div>
         
-        <p style="color:black;">
+        <p>
             <b>Skills:</b>
-            <p style="padding-left: 5%;">Python</p>
-            <p style="padding-left: 5%;">FullStack</p>
-            <p style="padding-left: 5%;">Java</p>
+            <p>Python</p>
+            <p>FullStack</p>
+            <p>Java</p>
         
         </p>
 
 
         
         <div class="outerbox">
-            <div className="box"class="para1" style="color: black;">Tamil</div>
-            <div className="box" class="para2" style="color: black;">English</div>
-            <div className="box" class="para3" style="color: black;">Hindi</div>
-            <div className="box" class="para4" style="color: black;">French</div>
+            <div class="box para1">Contact</div>
+            <div class="box para2">Mail</div>
+            <div class="box para3">LinkedIn</div>
+            <div class="box para4">Instagram</div>
             
-    </div>
+        </div>
             
         <p id="output"></p>
     
-    <button class="button" onClick="printMessage1()">Contact No</button>
-    <button class="button" onClick="printMessage2()">MailID</button>
-    <button class="button" onClick="printMessage3()">LinkedIn</button>
-    <button class="button" onClick="printMessage4()">InstaID</button>
+    <button class="button" onClick="printMessage1()">p1</button>
+    <button class="button" onClick="printMessage2()">p2</button>
+    <button class="button" onClick="printMessage3()">p3</button>
+    <button class="button" onClick="printMessage4()">p4</button>
     
 
      </div>
     
 
-    </>     
+</div>     
   );
 }
 
