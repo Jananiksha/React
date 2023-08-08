2dcc16256d65d6604b8e6123759de34bf65f98e8
Janani <105300710+Jananiksha@users.noreply.github.com>
2023-08-08 17:25:39 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,53 +4,59 @@ import './index.js';
 
 function App() {
   return (
-  <div>
-  <div>
-    <h1>WELCOME TO MY WEBSITE</h1>
-    </div>
-        
-
-    <h3> 
-       <b>I'm Jananiksha</b>
-    </h3>
-    
-    <h4>
-        Enthusiastic AI&DS student dedicated to lifelong learning and growth in the field of AI and Data Science.
-    </h4>
-    <div>
-    <h5>Education:</h5>
-        <p>
-            <p>Breeks All India Secondary School </p>
-            <p>AVB Matriculation Higher Secondary School</p>
-            <p>Kumaraguru College of Technology</p>
-        </p>
-
-    <h6>Hobbies:</h6>
-        <p>
-            <p>Listening to Music</p>
-            <p>Reading books</p>
-            <p>Travel</p>
-        </p>
-
-        
-    </div>
-        <div>
-    <h7>Skills:</h7>
-        <p>
-            <p>Python</p>
-            <p>FullStack</p>
-            <p>Java</p>
-        
-        </p>
 
 
-        <h8>Languages Known:</h8>
-        
-    </div>
-    <div>
-        
-    </div>
-    </div>
+    <><ul>
+          <li><a href="https://www.google.com/">HOME</a></li>
+          <li><a href="https://www.linkedin.com/in/jananiksha-p-666634231">LinkedIn</a></li>
+          
+      </ul><div>
+              <div>
+                  <h1>WELCOME TO MY WEBSITE</h1>
+              </div>
+
+
+              <h3>
+                  <b>I'm Jananiksha</b>
+              </h3>
+
+              <h4>
+                  Enthusiastic AI&DS student dedicated to lifelong learning and growth in the field of AI and Data Science.
+              </h4>
+              <div>
+                  <h5>Education:</h5>
+                  <p>
+                      <p>Breeks All India Secondary School </p>
+                      <p>AVB Matriculation Higher Secondary School</p>
+                      <p>Kumaraguru College of Technology</p>
+                  </p>
+
+                  <h6>Hobbies:</h6>
+                  <p>
+                      <p>Listening to Music</p>
+                      <p>Reading books</p>
+                      <p>Travel</p>
+                  </p>
+
+
+              </div>
+              <div>
+                  <h7>Skills:</h7>
+                  <p>
+                      <p>Python</p>
+                      <p>FullStack</p>
+                      <p>Java</p>
+
+                  </p>
+
+
+                  <h8>Languages Known:</h8>
+
+              </div>
+              <div>
+
+              </div>
+          </div></>
     );
 }
 
